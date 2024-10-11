2639827301f6f785655ff3d8976428b96926ba9e
mahsa <mahsarm99@gmail.com>
2024-10-11 16:43:04 +0330

    feat: add page query param to pagination

--- store/pagination.py
+++ store/pagination.py
@@ -4,4 +4,5 @@ from rest_framework.pagination import PageNumberPagination
 class CustomPagination(PageNumberPagination):
     page_size = 50
     page_size_query_param = "page_size"
+    page_query_param = "page"
     max_page_size = 100
