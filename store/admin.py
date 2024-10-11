40d6c81fc752f3bf79e7c79060dd5a5582646882
mahsa <mahsarm99@gmail.com>
2024-10-11 16:31:33 +0330

    feat: add tables to admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart, CartItem, Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "inventory")
+    search_fields = ("name",)
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "total_price",
+        "created_at",
+        "is_active",
+        "expiry_time",
+    )
+    search_fields = ("user__full_name",)
+    list_filter = ("is_active",)
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "cart", "product", "quantity")
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = [
+        "full_name",
+    ]
+    list_display = (
+        "id",
+        "full_name",
+        "phone_number",
+    )
+    readonly_fields = ("last_login",)
+    filter_horizontal = ("groups", "user_permissions")
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "phone_number",
+                    "password",
+                    "full_name",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {"fields": ("is_superuser", "is_staff", "groups", "user_permissions")},
+        ),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "full_name",
+                    "phone_number",
+                    "password1",
+                    "password2",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
