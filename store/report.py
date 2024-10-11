0317ce436f000cdc00ae6ec5fb48f620bf4edc3d
mahsa <mahsarm99@gmail.com>
2024-10-11 13:19:55 +0330

    feat: create a queryset to return daily cart prices reports

---
+++ store/report.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+
+from django.db.models import Sum
+from django.utils import timezone
+
+from .models import Cart
+
+
+def daily_cart_statistics(start_date, end_date):
+    if not start_date:
+        start_date = timezone.now() - timedelta(days=1)
+    if not end_date:
+        end_date = timezone.now()
+
+    start_date = start_date.date()
+    end_date = end_date.date()
+
+    queryset = (
+        Cart.objects.filter(created_at__date__range=[start_date, end_date])
+        .values(
+            "created_at__date",
+            "user__full_name",
+        )
+        .annotate(total_sum=Sum("total_price"))
+        .order_by("created_at__date", "-total_sum")
+    )
+
+    return queryset
