b6192cfcdb884723784f81315c0820d6173cc2e1
mahsa <mahsarm99@gmail.com>
2024-10-11 12:37:57 +0330

    feat: add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class User(AbstractUser):
+    phone_number = models.CharField(_("Phone number"), max_length=13, unique=True)
+    full_name = models.CharField(_("First name"), max_length=300)
+    username = None
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self) -> str:
+        return self.full_name
