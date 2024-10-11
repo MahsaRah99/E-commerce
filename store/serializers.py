bdd05a494511084dc9a2900d3b5dbb1901498ead
mahsa <mahsarm99@gmail.com>
2024-10-11 13:03:28 +0330

    feat: create cart item serializer

--- store/serializers.py
+++ store/serializers.py
@@ -7,3 +7,11 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ("name", "price")
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(many=True)
+
+    class Meta:
+        model = CartItem
+        fields = ("product", "quantity")
