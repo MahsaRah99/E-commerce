089e469cc235322775279e961b0e2440e502c449
mahsa <mahsarm99@gmail.com>
2024-10-14 12:35:55 +0330

    refactor: group carts by user_id in the report

--- store/report.py
+++ store/report.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-from django.db.models import Sum
+from django.db.models import F, Sum
 from django.utils import timezone
 
 from .models import Cart
@@ -16,9 +16,9 @@ def daily_cart_statistics(start_date, end_date):
         Cart.objects.filter(created_at__date__range=[start_date, end_date])
         .values(
             "created_at__date",
-            "user__full_name",
+            "user__id",
         )
-        .annotate(total_sum=Sum("total_price"))
+        .annotate(total_sum=Sum("total_price"), full_name=F("user__full_name"))
         .order_by("created_at__date", "-total_sum")
     )
 
